8fa4a1f15db895ebafa19819f0d306d92a2978d6
idilugurnal <idilugurnal@gmail.com>
2021-01-22 00:37:10 +0300

    Add cv to blog

--- pages/cv.js
+++ pages/cv.js
@@ -1,11 +1,13 @@
 import Head from "next/head";
 import Layout from "../components/Layout/layout";
+import React from 'react';
+import SinglePagePDFViewer from "../components/PDFViewer/SinglePagePDF";
 
 export default function Cv() {
   return (
     <Layout>
       <Head> CV </Head>
-      <p> This is my Cv here</p>
+        <SinglePagePDFViewer pdf={'./CV/current-cv.pdf'} />
     </Layout>
   );
 }
