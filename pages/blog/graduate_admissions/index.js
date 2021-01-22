f4f25cb829abfea215ce75258372e644e576f96d
idilugurnal <idilugurnal@gmail.com>
2021-01-22 16:45:01 +0300

    Refactor graduate_admissions page

--- pages/blog/graduate_admissions/index.js
+++ pages/blog/graduate_admissions/index.js
@@ -10,37 +10,37 @@ export default function Blogs() {
       </Head>
       <h1>Graduate Admissions</h1>
       <h3>
-        <Link href="/posts/graduate_admissions/done-with-college">
+        <Link href="/blog/graduate_admissions/done-with-college">
           <a> Done With College? What's Next?</a>
         </Link>
       </h3>
       <h3>
-        <Link href="/posts/graduate_admissions/preparing-applications">
+        <Link href="/blog/graduate_admissions/preparing-applications">
           <a> Preparing Your Applications in 6 Steps </a>
         </Link>
       </h3>
       <h3>
-        <Link href="/posts/graduate_admissions/application-agenda">
+        <Link href="/blog/graduate_admissions/application-agenda">
           <a> Application Agenda: Starting and Keeping Track </a>
         </Link>
       </h3>
       <h3>
-        <Link href="/posts/graduate_admissions/funding-studies">
+        <Link href="/blog/graduate_admissions/funding-studies">
           <a> Funding Your Studies: Scholarships & Grants </a>
         </Link>
       </h3>
       <h3>
-        <Link href="/posts/graduate_admissions/stay-motivated">
+        <Link href="/blog/graduate_admissions/stay-motivated">
           <a> How to Stay Motivated During COVID-19 </a>
         </Link>
       </h3>
       <h3>
-        <Link href="/posts/graduate_admissions/offers-and-rejections">
+        <Link href="/blog/graduate_admissions/offers-and-rejections">
           <a> A List of Offers and Rejections</a>
         </Link>
       </h3>
       <h3>
-        <Link href="/posts/graduate_admissions/whats-next">
+        <Link href="/blog/graduate_admissions/whats-next">
           <a> What's Next? </a>
         </Link>
       </h3>
