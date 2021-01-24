c8f5e2923804c028ea8eeb35a6960ff8a3ff8a77
idilugurnal <idilugurnal@gmail.com>
2021-01-24 19:55:04 +0300

    Add Layout to website with react bootstrap

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,67 +1,81 @@
 import Head from "next/head";
-import React from 'react';
+import React from "react";
 import styles from "./layout.module.scss";
 import utilStyles from "../../styles/utils.module.scss";
 import Link from "next/link";
-import NavigationBar from '../NavigationBar/NavigationBar'
+import NavigationBar from "../molecules/NavigationBar/NavigationBar";
+import { Container, Row, Col } from "react-bootstrap";
 
 const name = "Idil Ugurnal";
 export const siteTitle = "Idil Ugurnal Blog";
 
 export default function Layout({ children, home }) {
   return (
-      <div>
-          <div>
-              <NavigationBar  />
-          </div>
-        <div className={styles.container}>
-          <Head>
-            <link rel="icon" href="/favicon.ico" />
-            <meta
-              name="description"
-              content="Learn how to build a personal website using Next.js"
-            />
-            <meta
-              property="og:image"
-              content={`https://og-image.now.sh/${encodeURI(
-                siteTitle
-              )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-            />
-            <meta name="og:title" content={siteTitle} />
-            <meta name="twitter:card" content="summary_large_image" />
-          </Head>
-          <header className={styles.header}>
-            {home ? (
-              <>
-                <img
-                  src="/images/profile.jpg"
-                  className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-                  alt={name}
-                />
-                <h1 className={utilStyles.heading2Xl}>{name}</h1>
-              </>
-            ) : (
-              <>
-                <Link href="/">
-                  <a>
-                    <img
-                      src="/images/profile.jpg"
-                      className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                      alt={name}
-                    />
-                  </a>
-                </Link>
-                <h2 className={utilStyles.headingLg}>
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css"
+      />
+      <Container fluid>
+        <Row>
+          <Col xs={2}>
+            <NavigationBar />
+          </Col>
+          <Col xs={8} className={styles.container}>
+            <Head>
+              <link rel="icon" href="/favicon.ico" />
+              <meta
+                name="description"
+                content="My personal blog"
+              />
+              <meta
+                property="og:image"
+                content={`https://og-image.now.sh/${encodeURI(
+                  siteTitle
+                )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+              />
+              <meta name="og:title" content={siteTitle} />
+              <meta name="twitter:card" content="summary_large_image" />
+              <title> Blog </title>
+            </Head>
+            <header className={styles.header}>
+              {home ? (
+                <>
+                  <img
+                    src="/images/profile.jpg"
+                    className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+                    alt={name}
+                  />
+                  <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                </>
+              ) : (
+                <>
                   <Link href="/">
-                    <a className={utilStyles.colorInherit}>{name}</a>
+                    <a>
+                      <img
+                        src="/images/profile.jpg"
+                        className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                        alt={name}
+                      />
+                    </a>
                   </Link>
-                </h2>
-              </>
-            )}
-          </header>
-          <main>{children}</main>
-
-        </div>
-      </div>
+                  <h2 className={utilStyles.headingLg}>
+                    <Link href="/">
+                      <a className={utilStyles.colorInherit}>{name}</a>
+                    </Link>
+                  </h2>
+                </>
+              )}
+            </header>
+            <main>{children}</main>
+          </Col>
+            <Col xs={2}> </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
